d29f7597f38a932485a82ffde0982c97575ccac6
Alberto Estepa <alberto16166@gmail.com>
2025-02-19 20:09:53 +0100

    he añadido un fondo espacial

--- PruebaInterfazGrafica/src/Ventana.java
+++ PruebaInterfazGrafica/src/Ventana.java
@@ -1,10 +1,38 @@
 import javax.swing.*;
-
+import java.awt.*;
+import java.net.URL;
 public class Ventana  extends JFrame {
 
+    private Image fondo;
     public Ventana(){
-        setIconImage();
+        setTitle("Alberto Estepa");
+        ImageIcon icono =new ImageIcon(getClass().getResource("/fotoperfil.png"));
+        setIconImage(icono.getImage());
 
+        setSize (400,300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        cargarFondo();
+        setVisible(true);
     }
 
+
+    private void cargarFondo(){
+        //PARA HACER ESTO ES NECESARIO IMPORTAR JAVA.NET.URL
+        URL imageURL =getClass().getResource("/fondo.jpg");
+        if (imageURL!= null){
+            fondo =new ImageIcon(imageURL).getImage();
+        }else{
+            System.err.println("No se encontró la imagen");
+        }
+    }
+
+    @Override
+    public void paint (Graphics g){
+        super.paint(g);
+
+        if (fondo != null) {
+            g.drawImage(fondo, 0, 0, getWidth(), getHeight(), this);
+        }
+    }
 }
+
---
+++ PruebaInterfazGrafica/src/music/PlayMusic.java
@@ -0,0 +1,5 @@
+package music;
+import javax.sound.sampled.*;
+
+public class PlayMusic {
+}
